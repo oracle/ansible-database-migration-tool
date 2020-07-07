f275b835a35ded019495d7bd64a0b5391edf2270
lukasz_feldman <lukasz.feldman@oracle.com>
2020-07-07 15:51:06 +0200

    support for OCI automatic backup v3

--- setup_STEP3_backup_config.sh
+++ setup_STEP3_backup_config.sh
@@ -3,5 +3,5 @@
 #Copyright (c) 2020, Oracle and/or its affiliates.
 #The Universal Permissive License (UPL), Version 1.0
 #
-ansible-playbook setup_STEP3_backup_config.yml --module-path modules/ -i inventory --extra-vars @setup.json -vvv --step
+ansible-playbook setup_STEP3_backup_config.yml --module-path modules/ -i inventory --extra-vars @setup.json 
 
